eaced259985ceb7fc9ef3ffbccb47bf7abe726b9
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2021-04-07 23:16:05 +0300

    Implement search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import foodList from '../foodList';
 
-function SearchBar() {
+const SearchBar = () => {
+  const [input, setInput] = useState('');
+
+  const updateInput = (input) => {
+    const filtered = foodList.filter((food) => {
+      return food.name.toLowerCase().includes(input.toLowerCase());
+    });
+    setInput(input);
+    console.log(filtered.map((x) => x.name));
+  };
+
   return (
-    <form className="search-bar pb-4">
-        <label htmlFor="query" className="label sr-only">Food Name</label>
-        <input type="text" name="query" className="input w-full text-2xl px-4 py-2 rounded-3xl border border-gray-500 mb-4" placeholder="Search..."/>
-        <button type="submit" className="button w-full text-white leading-loose bg-gray-600 border border-black rounded-3xl px-4 py-2 text-1xl transition-colors hover:bg-gray-800">Search</button>
-    </form>
+    <div className="search-bar pb-4">
+      <label htmlFor="query" className="label sr-only">
+        Food Name
+      </label>
+      <input
+        type="text"
+        name="query"
+        className="input w-full text-2xl px-4 py-2 rounded-3xl border border-gray-500 mb-4"
+        placeholder="Search..."
+        value={input}
+        onChange={(e) => updateInput(e.target.value)}
+      />
+    </div>
   );
-}
+};
 
 export default SearchBar;
