19f0749df4365bb51dc9ea2dc9a99d4c961320e3
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2021-04-07 23:15:31 +0300

    Add demo foodList

---
+++ src/foodList.js
@@ -0,0 +1,36 @@
+const foodList = [
+  {
+    name: 'banana',
+    canEat: 'Yes',
+    level: 'In Moderation',
+    description:
+      'Bananas are a wonderful snack for your dog with many health benefits. Filled with essential vitamins and nutrients, they are tasty as well and most dogs love them! However, since they contain high sugar content, they should only be an occasional human food treat to avoid weight gain, stomach upset, or constipation in your dog.',
+    tips: 'Always remove the peel, ripe banana, slice up the banana into small pieces'
+  },
+  {
+    name: 'apple',
+    canEat: 'Yes',
+    level: 'In Moderation',
+    description:
+      'Apple slices make a delicious, healthy snack for your dog and can help keep your dog’s teeth clean and their breath fresh. However, the core of the apple and the apple seeds especially can be harmful to dogs. Apple seeds contain a small amount of cyanide which is released when they’re broken or chewed. Just swallowing a few apple pips is unlikely to cause your dog any harm, but it’s best to remove them and avoid the risk.',
+    tips: 'Cut up the apple into easily digestible slices, remove the seeds, stem and core'
+  },
+  {
+    name: 'chocolate',
+    canEat: 'No',
+    level: 'Toxic',
+    description:
+      'Chocolate is poisonous to dogs mostly because of its theobromine content, which dogs are unable to metabolize effectively. If your dog eats chocolate, you should monitor them closely and seek veterinary attention if they show any symptoms, or if they are very young, pregnant or have other health concerns.',
+    tips: ''
+  },
+  {
+    name: 'almonds',
+    canEat: 'No',
+    level: 'Harmful',
+    description:
+      'Properly processed almonds are not directly toxic to your dog but the canine system does not properly digest the proteins in nuts. Eating large quantities of almonds, whether all at once or frequent smaller amounts given regularly, can cause gastrointestinal distress, obesity, and pancreatitis. In addition to the dangers posed by the almonds themselves, they may also harbor Aspergillus mold. Dogs are particularly susceptible to the aflatoxin produced by this mold and exposure can have serious consequences for your pet.',
+    tips: ''
+  }
+];
+
+export default foodList;
