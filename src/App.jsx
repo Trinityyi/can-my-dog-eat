dda4752f11acf10b26c0cb17094ead9355dd12a1
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2021-04-07 23:15:15 +0300

    Format files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import React from 'react';
 import SearchBar from './components/SearchBar';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="app flex flex-col items-center p-8">
       <h1 className="title text-7xl pb-8">Can My Dog Eat...</h1>
-      <SearchBar/>
+      <SearchBar />
     </div>
   );
-}
+};
 
 export default App;
