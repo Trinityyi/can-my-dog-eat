438d292cffdc95474d89e6d7023740b83fbf8d9d
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2021-04-10 23:38:42 +0300

    Add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Search from './components/Search';
+import Footer from './components/Footer';
 import './App.css';
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <div className="app flex flex-col items-center p-8">
       <h1 className="title text-7xl pb-8">Can My Dog Eat...</h1>
       <Search />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+  return <div className="footer"></div>;
+};
+
+export default Footer;
