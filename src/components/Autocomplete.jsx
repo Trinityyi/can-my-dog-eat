78e81b9edc9a59ec4513a078074eaeda8427700d
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2021-04-10 21:50:23 +0300

    Create Autocomplete navigation

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 
-const Autocomplete = ({ suggestions, onClick }) => {
+const Autocomplete = ({ suggestions, onClick, selectedIndex }) => {
   if (!suggestions) return null;
   return (
-    <div className="autocomplete px-2 py-1 absolute z-10 w-full bg-white rounded-b-lg rounded-t-none border border-t-0 border-gray-500">
+    <div className="autocomplete py-1 absolute z-10 w-full bg-white rounded-b-lg rounded-t-none border border-t-0 border-gray-500">
       {!suggestions.length ? (
         <div>No suggestion</div>
       ) : (
         <ul>
-          {suggestions.map((x) => (
+          {suggestions.map((x, i) => (
             <li className="block" key={x}>
-              <button onClick={() => onClick(x)}>{x}</button>
+              <button
+                className={`w-full text-left p-2 transition-colors hover:bg-blue-500 hover:text-white focus:bg-blue-500 focus:text-white ${
+                  i === selectedIndex ? 'bg-blue-500 text-white' : ''
+                }`}
+                onClick={() => onClick(x)}
+              >
+                {x}
+              </button>
             </li>
           ))}
         </ul>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,22 +8,39 @@ const Search = () => {
   const [input, setInput] = useState('');
   const [filteredList, setFilteredList] = useState(null);
   const [result, setResult] = useState();
+  const [pointer, setPointer] = useState(-1);
 
   const updateInput = (input) => {
+    const inputString = input.trim().toLowerCase();
     const filtered = foodList.filter((food) => {
-      return food.name.toLowerCase().includes(input.toLowerCase());
+      return food.name.toLowerCase().includes(inputString);
     });
     setInput(input);
-    setFilteredList(filtered);
+    setFilteredList(inputString === '' ? null : filtered);
+    setPointer(-1);
   };
 
   const updateResults = (input) => {
     const filtered = foodList.find((food) => {
-      return food.name.toLowerCase().includes(input.toLowerCase());
+      return food.name.toLowerCase() === input.toLowerCase();
     });
     setInput(input);
     setResult(filtered);
     setFilteredList(null);
+    setPointer(-1);
+  };
+
+  const handleNavigation = (key) => {
+    if (!filteredList) return;
+    if (key === 'ArrowDown') {
+      let newPointer = pointer + 1;
+      if (newPointer >= filteredList.length) newPointer = 0;
+      setPointer(newPointer);
+    } else if (key === 'ArrowUp') {
+      let newPointer = pointer - 1;
+      if (newPointer < 0) newPointer = filteredList.length - 1;
+      setPointer(newPointer);
+    }
   };
 
   return (
@@ -32,11 +49,18 @@ const Search = () => {
         className={`search-input rounded-lg mb-5 relative ${
           !filteredList ? '' : 'with-autocomplete'
         }`}
+        onKeyUp={(e) => handleNavigation(e.key)}
+        onKeyPress={(e) => {
+          if (e.key === 'Enter') {
+            updateResults(pointer === -1 ? input : filteredList[pointer].name);
+          }
+        }}
       >
         <SearchBar input={input} onChange={updateInput} />
         <Autocomplete
           suggestions={!filteredList ? null : filteredList.map((x) => x.name)}
           onClick={(x) => updateResults(x)}
+          selectedIndex={pointer}
         />
       </div>
       <SearchResults results={result} />
