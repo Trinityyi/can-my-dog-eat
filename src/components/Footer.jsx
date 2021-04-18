c0fb6be7787bf9b1c4a81824e0347a5a040e7b9b
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2021-04-18 23:27:48 +0300

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer p-8 w-full max-w-2xl mx-auto">
+      <p className="disclaimer pb-6 text-sm">
+        This website does not provide medical advice. The information, including but not limited to,
+        text, graphics, images and other materials contained on this website are for informational
+        purposes only. No material on this site is intended to be a substitution for professional
+        veterinarian advice, diagnosis or treatment. Always seek the advice of your veterinarian
+        with any questions you may have regarding how to feed or medicate your pet. Never disregard
+        professional advice or delay in seeking it because of something you have read on this
+        website!
+      </p>
+      <p className="text-sm">
+        Designed and developed by{' '}
+        <a href="https://github.com/Trinityyi" className="underline">
+          Isabelle Viktoria Maciohsek
+        </a>{' '}
+        &copy; {new Date().getFullYear()} /{' '}
+        <a href="https://github.com/Trinityyi/can-my-dog-eat" className="underline">
+          Source code
+        </a>
+        <br /> Built with{' '}
+        <a href="https://reactjs.org/" rel="nofollow noreferrer">
+          ReactJS
+        </a>
+        , icons made by{' '}
+        <a href="https://feathericons.com/" title="Feather" rel="nofollow noreferrer">
+          Feather
+        </a>{' '}
+        and{' '}
+        <a href="https://www.freepik.com" title="Freepik" rel="nofollow noreferrer">
+          Freepik
+        </a>{' '}
+        from{' '}
+        <a href="https://www.flaticon.com/" title="Flaticon" rel="nofollow noreferrer">
+          www.flaticon.com
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
